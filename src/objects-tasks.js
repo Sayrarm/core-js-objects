10fab7378f99c8e99ad4108717526e026ac898ae
Olga D. <minsecond@mail.ru>
2025-02-20 16:26:16 +0300

    feat: add function makeImmutable

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -150,8 +150,13 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  Object.freeze(obj);
+  return obj;
 }
 
 /**
