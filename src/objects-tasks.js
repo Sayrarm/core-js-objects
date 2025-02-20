cc8a975c207e1fda5eed02408b8eed5a684bc411
Olga D. <minsecond@mail.ru>
2025-02-20 16:37:27 +0300

    fix: fix function Rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -257,7 +257,7 @@ function Rectangle(width, height) {
   this.width = width;
   this.height = height;
 
-  this.getArea = function () {
+  this.getArea = function calculateArea() {
     return this.width * this.height;
   };
 }
